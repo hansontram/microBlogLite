5130853bc2d7e1c8e682225aad2e907fa3efd55f
Hanson Tram <htram.dev@gmail.com>
2024-01-02 09:05:54 -0700

    log out works on all pages

---
+++ microbloglite-capstone-starter-frontend/addPosts/addPosts.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+    authService = new AuthService();
+  
+    const logoutLink = document.querySelector("#logout-link");
+    logoutLink.addEventListener("click", () => {
+      authService.logout();
+    });
+    // moved here from posts.html to avoid console error
+    if (authService.isLoggedIn() === false) {
+      window.location.replace("/");
+    }
+  });
