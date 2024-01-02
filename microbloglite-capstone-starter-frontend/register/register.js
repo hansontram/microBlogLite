f1177d72ed094546c389dd63ec6a9bb00d9680e2
0zoneGG <jcrojas408@gmail.com>
2024-01-02 11:20:57 -0700

    Completed registerpage

--- microbloglite-capstone-starter-frontend/register/register.js
+++ microbloglite-capstone-starter-frontend/register/register.js
@@ -1,26 +1,20 @@
-let authService
-let registerForm
- 
-document.addEventListener("DOMContentLoaded",()=>{
+let authService;
+let registerForm;
 
-
-    authService= new AuthService();
-    registerForm =  document.querySelector("#register")
-    console.log(registerForm)
-    registerForm.addEventListener("submit", register)
-})
-function register (event){
-
-    event.preventDefault()
-    const formData = {
-        username : registerForm.username.value.trim(),
-        fullname: registerForm.fullname.value.trim(),
-        password: registerForm.password.value.trim()
-    };
-    console.log (formData.username)
-registerForm.submit.disabled = true
-authService.register(formData)
+document.addEventListener("DOMContentLoaded", () => {
+  authService = new AuthService();
+  registerForm = document.querySelector("#register");
+  console.log(registerForm);
+  registerForm.addEventListener("submit", register);
+});
+function register(event) {
+  event.preventDefault();
+  const formData = {
+    username: registerForm.username.value.trim(),
+    fullName: registerForm.fullname.value.trim(),
+    password: registerForm.password.value.trim(),
+  };
+  console.log(formData.username);
+  registerForm.submit.disabled = true;
+  authService.register(formData);
 }
-       
-
-        
